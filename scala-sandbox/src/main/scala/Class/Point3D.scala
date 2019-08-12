0c1b273a22af772762457caeb95095e36ce46816
closekn <close.me.kn@gmail.com>
2019-08-12 22:57:42 +0900

    finish Class

---
+++ scala-sandbox/src/main/scala/Class/Point3D.scala
@@ -0,0 +1,5 @@
+package Class
+
+class Point3D(val x:Int, val y:Int, val z:Int) {
+
+}
---
+++ scala-sandbox/src/main/scala/Class/main.scala
@@ -0,0 +1,10 @@
+package Class
+
+object main {
+  def main(args: Array[String]): Unit = {
+    val p = new Point3D(10, 20, 30)
+    println(p.x) // 10
+    println(p.y) // 20
+    println(p.z) // 30
+  }
+}
