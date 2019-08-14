09533dc5294a14813656c866325338b95f006458
closekn <close.me.kn@gmail.com>
2019-08-14 16:03:08 +0900

    finish Implicit

---
+++ scala-sandbox/src/main/scala/Implicit/Additives.scala
@@ -0,0 +1,33 @@
+package Implicit
+
+object Additives {
+  trait Additive[A] {
+    def plus(a: A, b: A): A
+    def zero: A
+  }
+
+  implicit object StringAdditive extends Additive[String] {
+    def plus(a: String, b: String): String = a + b
+    def zero: String = ""
+  }
+
+  implicit object IntAdditive extends Additive[Int] {
+    def plus(a: Int, b: Int): Int = a + b
+    def zero: Int = 0
+  }
+
+  case class Point(x: Int, y: Int)
+
+  implicit object PointAdditive extends Additive[Point] {
+    def plus(a: Point, b: Point): Point = Point(a.x + b.x, a.y + b.y)
+    def zero: Point = Point(0, 0)
+  }
+
+  def sum[A](lst: List[A])(implicit m: Additive[A]) = lst.foldLeft(m.zero)((x, y) => m.plus(x, y))
+
+  def main(args: Array[String]): Unit = {
+    println(sum(List(1, 2, 3)))
+    println(sum(List("T", "e", "st", "!")))
+    println(sum(List(Point(1, 2), Point(3, 4), Point(5, 6))))
+  }
+}
